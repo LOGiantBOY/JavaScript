0e922039e5c4176a930fb478ff1a51288b803dbd
Luke1803 <lkogenhop@gmail.com>
2020-01-15 10:51:37 +0100

    Initial commit

---
+++ H01/MijJavaScript.js
@@ -0,0 +1,14 @@
+console.log("Hello world!");
+
+for (var i = 0; i < 10; i++) {
+    console.log("i heeft nu de waarde " + i)
+}
+
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    wielen: 4,
+    kleur: "Blauw"
+}
+
+console.log(mijnauto.kleur);
