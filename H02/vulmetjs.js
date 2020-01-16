9457add09b312d319a8316ba70a6b40b093b7dc1
Luke1803 <lkogenhop@gmail.com>
2020-01-16 13:25:32 +0100

    netlify link

---
+++ H02/vulmetjs.js
@@ -0,0 +1,6 @@
+document.getElementById("paginatitel").innerText = "Dit is een paginatitle";
+document.getElementById("subkop").innerText = "Dit is een subkop";
+document.getElementById("formule1").src = "img/F1.jpg";
+document.getElementById("formule2").src = "img/F1.2.jpg";
+
+
