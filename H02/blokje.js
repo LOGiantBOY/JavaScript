b67677c7a70ca2b32ec061b3603f8beb6754ca03
Luke1803 <lkogenhop@gmail.com>
2020-01-20 13:53:58 +0100

    hoofdstuk 2 blokjes

---
+++ H02/blokje.js
@@ -0,0 +1,29 @@
+// div colors geven
+document.getElementById("bl1").style.backgroundColor = "#00ccff";
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl4").style.backgroundColor = "red";
+// div 100 bij 100 px maken
+document.getElementById("bl1").style.height = "100px";
+document.getElementById("bl1").style.width = "100px";
+
+document.getElementById("bl2").style.height = "100px";
+document.getElementById("bl2").style.width = "100px";
+
+document.getElementById("bl3").style.height = "100px";
+document.getElementById("bl3").style.width = "100px";
+
+document.getElementById("bl4").style.height = "100px";
+document.getElementById("bl4").style.width = "100px";
+//Margin top
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl3").style.marginTop = "50px";
+// text goed zetten
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("bl3").style.textAlign = "right";
+
+
+
+
+
