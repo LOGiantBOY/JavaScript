9781160c36994c3e31ca8e027b0863aab7e85151
Luke1803 <lkogenhop@gmail.com>
2020-01-29 14:10:42 +0100

    netlify link + h4 bijna klaar + h5 is af

---
+++ H04/imageslider.js
@@ -0,0 +1,17 @@
+var plaatjes = [1,2,3,4,5,6,7,8,9];
+var teller = 0;
+var slideholder = document.getElementById("slideholder");
+slideholder.style.backgroundImage = "url('img/lion1.jpg')";
+
+slideholder.addEventListener("click", function () {
+    slideholder.style.backgroundImage = "url('img/lion" + getLion() + ".jpg')";
+});
+function getLion() {
+    if (teller >= plaatjes.length) {
+        teller = 1;
+    } else {
+        teller++;
+    }
+    console.log(teller);
+    return teller;
+}
---
+++ H05/chooser.js
@@ -0,0 +1,39 @@
+pics = document.getElementById("pics");
+createPicsHolders();
+createLionImages();
+
+function createPicsHolders() {
+    for (var i = 0; i < 9 ; i++) {
+        pictureHolder = document.createElement("div");
+        pictureHolder.className = "picture-holder";
+        pictureHolder.id = "picture-holder-" + i;
+        pics.appendChild(pictureHolder);
+    }
+}
+
+function createLionImages() {
+    pictureHolders = document.getElementsByClassName("picture-holder");
+    for (var i = 0; i < pictureHolders.length; i++) {
+        favoriet = document.createElement("div");
+        favoriet.className = "favoriet";
+        favoriet.id = "favoriet_" + (i+1);
+        lionPlaatje = document.createElement("img");
+        lionPlaatje.src = "img/lion" + (i+1) + ".jpg";
+        lionPlaatje.id = (i+1);
+        lionPlaatje.addEventListener("click", function () {
+            maakFavoriet(this.id);
+        });
+        pictureHolders[i].appendChild(favoriet);
+        pictureHolders[i].appendChild(lionPlaatje);
+    }
+}
+function maakFavoriet(id) {
+    nosofavoriet = document.getElementsByClassName("favoriet");
+
+    for (var i = 0; i < nosofavoriet.length; i++) {
+        nosofavoriet[i].style.backgroundImage = "none";
+    }
+
+    favoriet = document.getElementById("favoriet_" + id);
+    favoriet.style.backgroundImage = "url('img/hart1.png')";
+}
